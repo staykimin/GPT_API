585b6e380784f7f1fa9f024b726069667839e89e
staykimin <67236270+staykimin@users.noreply.github.com>
2024-03-19 14:46:28 +0700

    Update core_driver.py

--- Source Code/kimin/core_driver.py
+++ Source Code/kimin/core_driver.py
@@ -7,12 +7,12 @@ class Driver:
 		hasil = {'status':False}
 		try:
 			if kimin.parameter['method'].lower() == 'get':
-				respon = requests.request(kimin.parameter['method'], kimin.parameter['url'], headers=kimin.parameter['header'])
+				respon = requests.request(kimin.parameter['method'], kimin.parameter['url'], headers=kimin.parameter.get('header', {}))
 			elif kimin.parameter['method'].lower() == 'post' or 'patch':
 				if kimin.parameter['data_type'].lower() == 'json':
-					respon = requests.request(kimin.parameter['method'], kimin.parameter['url'], headers=kimin.parameter['header'], json=kimin.parameter['data'])
+					respon = requests.request(kimin.parameter['method'], kimin.parameter['url'], headers=kimin.parameter.get('header', {}), json=kimin.parameter['data'])
 				elif kimin.parameter['data_type'].lower() == 'form':
-					respon = requests.request(kimin.parameter['method'], kimin.parameter['url'], headers=kimin.parameter['header'], data=kimin.parameter['data'])
+					respon = requests.request(kimin.parameter['method'], kimin.parameter['url'], headers=kimin.parameter.get('header', {}), data=kimin.parameter['data'])
 			hasil['status'], hasil['data'], hasil['status_code'], hasil['respon']= True, respon.text, respon.status_code, respon
 		except requests.Timeout:
 			hasil['data'] = "Timeout"
